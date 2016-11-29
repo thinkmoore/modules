02dd1e0bce94ed7ad300aab0a461be98b69586fe
Scott Moore <scott@thinkmoore.net>
2016-11-29 08:27:19 -0800

    progress on units model

--- units.rkt
+++ units.rkt
@@ -11,9 +11,9 @@
   (e ::= v x (e e ...) (if0 e e e)
          (compound (import value-var-decl ...)
                    (export value-var-decl ...)
-                   (link e with value-var-decl ... provides value-var-decl ...)
-                   (and e with value-var-decl ... provides value-var-decl ...))
-         (invoke e with value-invoke-link ...)
+                   (link e with (value-var-decl ...) provides (value-var-decl ...))
+                   (and e with (value-var-decl ...) provides (value-var-decl ...)))
+         (invoke e with (value-invoke-link ...))
          (seq e e) (letrec (value-defn value-defn ...) e))
   (value-defn ::= (val x = e))
   (value-invoke-link ::= (x = e))
@@ -25,15 +25,15 @@
                  (and e with value-var-decl ... provides value-var-decl ...))
        (compound (import value-var-decl ...)
                  (export value-var-decl ...)
-                 (link v with value-var-decl ... provides value-var-decl ...)
-                 (and E with value-var-decl ... provides value-var-decl ...))
-       (invoke E with (x = e) ...)
-       (invoke v with (x = v) ... (x = E) (x = e) ...)
+                 (link v with (value-var-decl ...) provides (value-var-decl ...))
+                 (and E with (value-var-decl ...) provides (value-var-decl ...)))
+       (invoke E with ((x = e) ...))
+       (invoke v with ((x = v) ... (x = E) (x = e) ...))
        (seq E e))
   (x ::= variable-not-otherwise-mentioned)
   #:binding-forms
   (λ (x ...) e #:refers-to (shadow x ...))
-  (unit (import x_1 ...)
+  #;(unit (import x_1 ...)
         (export x_2 ...)
         (val x_3 = e_3 #:refers-to (shadow x_1 ...)) ...
         e #:refers-to (shadow x_1 ...))
@@ -50,6 +50,31 @@
   letrec-expr : (value-defn ...) e -> e
   [(letrec-expr (value-defn ...) e)
    (letrec (value-defn ...) e)])
+
+(define-judgment-form units
+  #:mode (∈ O I)
+  #:contract (∈ e (e ...))
+  [---------------------
+   (∈ e_1 (e_1 e_2 ...))]
+  [(∈ e_1 (e_3 ...))
+   ---------------------
+   (∈ e_1 (e_2 e_3 ...))])
+
+(define-judgment-form units
+  #:mode (⊆ I I)
+  #:contract (⊆ (e ...) (e ...))
+  [--------------
+   (⊆ () (e ...))]
+  [(∈ e_1 (e_3 ...))
+   (⊆ (e_2 ...) (e_3 ...))
+   -----------------------------
+   (⊆ (e_1 e_2 ...) (e_3 ...))])
+
+(define-judgment-form units
+  #:mode (distinct I)
+  #:contract (distinct (e ...))
+  [----------------------------
+   (distinct (e_!_1 ... e_!_1))])
 
 (define units-red
   (reduction-relation
@@ -77,21 +102,36 @@
                                          (val x_d_2 = e_d_2)
                                          ...
                                          e_2)
-                                   with x_w_2 ... provides x_p_2 ...)
-                             (link (unit (import x_i_3 ...)
+                                   with (x_w_2 ...) provides (x_p_2 ...))
+                             (and  (unit (import x_i_3 ...)
                                          (export x_e_3 ...)
                                          (val x_d_3 = e_d_3)
                                          ...
                                          e_3)
-                                   with x_w_3 ... provides x_p_3 ...)))
-        (in-hole E 0)
+                                   with (x_w_3 ...) provides (x_p_3 ...))))
+        (in-hole E (unit (import x_i_1 ...)
+                         (export x_e_1 ...)
+                         (val x_d_2 = e_d_2)
+                         ...
+                         (val x_d_3 = e_d_3)
+                         ...
+                         (seq e_2 e_3)))
+        (judgment-holds (⊆ (x_i_2 ...) (x_w_2 ...)))
+        (judgment-holds (⊆ (x_p_2 ...) (x_e_2 ...)))
+        (judgment-holds (⊆ (x_i_3 ...) (x_w_3 ...)))
+        (judgment-holds (⊆ (x_p_3 ...) (x_e_3 ...)))
+        (judgment-holds (distinct (x_i_1 ... x_d_2 ... x_d_3 ...)))
         "compound")
    (--> (in-hole E (invoke (unit (import x_i ...)
                                  (export x_e ...)
                                  (val x_d = e_d)
                                  ...
-                                 e_b) with (x_w = v_w) ...))
-        (in-hole E 0)
+                                 e_b) with ((x_w = v_w) ...)))
+        (in-hole E (subst (letrec ((val x_d = e_d) ...)
+                            e_b)
+                          (x_w ...)
+                          (v_w ...)))
+        (judgment-holds (⊆ (x_i ...) (x_w ...)))
         "invoke")
    (--> (in-hole E (seq v e))
         (in-hole E e)
@@ -104,3 +144,48 @@
   (term (letrec ((val even? = (λ (x) (if0 x 1 (odd?  (- x 1)))))
                  (val odd?  = (λ (x) (if0 x 0 (even? (- x 1))))))
           (even? 3))))
+
+(define unit-test
+  (term (invoke (compound (import)
+                          (export)
+                          (link (unit (import odd?)
+                                      (export even?)
+                                      (val even? = (λ (x) (if0 x 1 (odd?  (- x 1)))))
+                                      0)
+                                with (odd?) provides (even?))
+                          (and  (unit (import even?)
+                                      (export odd?)
+                                      (val odd?  = (λ (x) (if0 x 0 (even? (- x 1)))))
+                                      (even? 4))
+                                with (even?) provides (odd?)))
+                with ())))
+
+(define compound-test
+  (term (compound (import)
+                  (export)
+                  (link (unit (import odd?)
+                              (export even?)
+                              (val even? = (λ (x) (if0 x 1 (odd?  (- x 1)))))
+                              0)
+                        with (odd?) provides (even?))
+                  (and  (unit (import even?)
+                              (export odd?)
+                              (val odd?  = (λ (x) (if0 x 0 (even? (- x 1)))))
+                              (even? 4))
+                        with (even?) provides (odd?)))))
+
+(define shadow-test
+  (term ((λ (odd?)
+          (compound (import)
+                  (export)
+                  (link (unit (import odd?)
+                              (export even?)
+                              (val even? = (λ (x) (if0 x 1 (odd?  (- x 1)))))
+                              0)
+                        with (odd?) provides (even?))
+                  (and  (unit (import even?)
+                              (export odd?)
+                              (val odd?  = (λ (x) (if0 x 0 (even? (- x 1)))))
+                              (even? 4))
+                        with (even?) provides (odd?))))
+         (λ (x) 42))))
